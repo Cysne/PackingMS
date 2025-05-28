20b826e1cafe802d503aa7de3da56553c8da4857
liziane cysne <lizianecysne1@gmail.com.>
2025-05-27 23:04:19 -0300

    feat: Adicionar DTO ProductDTO para representar informações de produtos

---
+++ PackingService.Api/DTOs/ProductDTO.cs
@@ -0,0 +1,8 @@
+
+public class ProductDTO
+{
+    public string Name { get; set; }
+    public decimal Height { get; set; }
+    public decimal Width { get; set; }
+    public decimal Length { get; set; }
+}
