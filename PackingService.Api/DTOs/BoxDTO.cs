e3a1ec455c096a317fb67d57d02e9ccfd2ffd5e3
liziane cysne <lizianecysne1@gmail.com.>
2025-05-27 23:01:57 -0300

    feat: Adicionar DTO BoxDTO para representar informações de caixas

---
+++ PackingService.Api/DTOs/BoxDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace PackingService.Api.DTOs
+{
+    public class BoxDTO
+    {
+        public string BoxType { get; set; }
+        public decimal Height { get; set; }
+        public decimal Width { get; set; }
+        public decimal Length { get; set; }
+    }
+}
