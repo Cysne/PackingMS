447c6411c73f75b148c9a23ffa6c73e62c7b225b
liziane cysne <lizianecysne1@gmail.com.>
2025-05-27 23:04:34 -0300

    feat: Adicionar entidade OrderItemEntity para representar itens de pedidos

---
+++ PackingService.Api/Entities/OrderItemEntity.cs
@@ -0,0 +1,12 @@
+namespace PackingService.Api.Entities
+{
+    public class OrderItemEntity
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public OrderEntity? Order { get; set; }
+        public int ProductId { get; set; }
+        public ProductEntity? Product { get; set; }
+        public int Quantity { get; set; }
+    }
+}
