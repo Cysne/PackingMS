885c90839860fff58b17f7083e2514c4e4e044df
liziane cysne <lizianecysne1@gmail.com.>
2025-05-27 22:57:25 -0300

    feat: Adicionar interface IPackingService para empacotamento de pedidos

---
+++ PackingService.Api/Services/IPackingService.cs
@@ -0,0 +1,10 @@
+using PackingService.Api.DTOs;
+using System.Collections.Generic;
+
+namespace PackingService.Api.Services
+{
+    public interface IPackingService
+    {
+        List<OrderPackingResultDTO> PackOrders(IEnumerable<OrderDTO> orders);
+    }
+}
