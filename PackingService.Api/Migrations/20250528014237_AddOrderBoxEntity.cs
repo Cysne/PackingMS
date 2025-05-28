acde09b826c357435f979aece689c2ec559547f5
liziane cysne <lizianecysne1@gmail.com.>
2025-05-27 23:05:11 -0300

    feat: Adicionar migração para criar a tabela OrderBoxes com relacionamentos

---
+++ PackingService.Api/Migrations/20250528014237_AddOrderBoxEntity.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PackingService.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderBoxEntity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OrderBoxes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderId = table.Column<int>(type: "int", nullable: false),
+                    BoxId = table.Column<int>(type: "int", nullable: false),
+                    Observation = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OrderBoxes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_OrderBoxes_Boxes_BoxId",
+                        column: x => x.BoxId,
+                        principalTable: "Boxes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_OrderBoxes_Orders_OrderId",
+                        column: x => x.OrderId,
+                        principalTable: "Orders",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderBoxes_BoxId",
+                table: "OrderBoxes",
+                column: "BoxId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderBoxes_OrderId",
+                table: "OrderBoxes",
+                column: "OrderId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OrderBoxes");
+        }
+    }
+}
