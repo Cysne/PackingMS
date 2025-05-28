51a38f6807fed4bad228c210c06bd29e5e584c90
liziane cysne <lizianecysne1@gmail.com.>
2025-05-27 23:04:23 -0300

    feat: Adicionar entidade BoxEntity para representar informações de caixas

---
+++ PackingService.Api/Entities/BoxEntity.cs
@@ -0,0 +1,13 @@
+using System.Collections.Generic;
+namespace PackingService.Api.Entities
+{
+    public class BoxEntity
+    {
+        public int Id { get; set; }
+        public string? BoxType { get; set; }
+        public decimal Height { get; set; }
+        public decimal Width { get; set; }
+        public decimal Length { get; set; }
+        public ICollection<OrderBoxEntity>? OrderBoxes { get; set; }
+    }
+}
