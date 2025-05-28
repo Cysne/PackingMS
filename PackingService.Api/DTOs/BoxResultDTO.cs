4f333ad96eb9e81c12f1bf3d3ff3c71237537753
liziane cysne <lizianecysne1@gmail.com.>
2025-05-27 23:02:03 -0300

    feat: Adicionar DTO BoxResultDTO para representar resultados de caixas

---
+++ PackingService.Api/DTOs/BoxResultDTO.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace PackingService.Api.DTOs
+{
+    public class BoxResultDTO
+    {
+        public string? box_id { get; set; }
+        public List<string> products { get; set; } = new List<string>();
+        public string? observation { get; set; }
+    }
+}
