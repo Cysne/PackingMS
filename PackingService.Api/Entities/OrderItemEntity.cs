ae44f3690649cc81409b6278a123e7d8dc952c83
Liziane Cysne <lizianecysne1@gmail.com>
2025-05-29 14:34:36 +0000

    fix: Renomear propriedade Id para OrderItemId e ajustar referências na entidade OrderItemEntity

--- PackingService.Api/Entities/OrderItemEntity.cs
+++ PackingService.Api/Entities/OrderItemEntity.cs
@@ -2,11 +2,13 @@ namespace PackingService.Api.Entities
 {
     public class OrderItemEntity
     {
-        public int Id { get; set; }
+        public int OrderItemId { get; set; }
+
         public int OrderId { get; set; }
-        public OrderEntity? Order { get; set; }
+        public OrderEntity Order { get; set; }
         public int ProductId { get; set; }
-        public ProductEntity? Product { get; set; }
+        public ProductEntity Product { get; set; }
+
         public int Quantity { get; set; }
     }
 }
