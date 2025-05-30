a2021eecd0f4f3499358259d79230a9fae3b0475
Liziane Cysne <lizianecysne1@gmail.com>
2025-05-30 15:53:16 +0000

    feat: adicionar configuração de autenticação JWT e middleware de autorização

--- PackingService.Api/Program.cs
+++ PackingService.Api/Program.cs
@@ -6,6 +6,9 @@ using PackingService.Api.DTOs;
 using PackingService.Api.Services;
 using PackingService.Api.Strategies;
 using Swashbuckle.AspNetCore.SwaggerUI;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 
 Log.Logger = new LoggerConfiguration()
@@ -20,8 +23,29 @@ builder.Host.UseSerilog();
 builder.Services.AddDbContext<PackingDbContext>(opts =>
     opts.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+var jwtKey = builder.Configuration["Jwt:Key"] ?? "SuperSecretKeyWithAtLeast32Characters123!";
+var key = Encoding.UTF8.GetBytes(jwtKey);
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
 
 builder.Services.AddScoped<IPackingService, PackingService.Api.Services.PackingService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddSingleton<IPackingStrategy, FirstFitDecreasingPackingStrategy>();
 builder.Services.AddSingleton<IEnumerable<BoxDTO>>(sp => new[]
 {
@@ -36,13 +60,36 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Packing API", Version = "v1" });
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = "JWT Authorization header usando Bearer scheme. Example: 'Bearer {token}'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
 });
 
 var app = builder.Build();
 
-// Middleware para tratar as exceções da api
 app.UseMiddleware<PackingService.Api.Middleware.ExceptionMiddleware>();
-
 
 using (var scope = app.Services.CreateScope())
 {
@@ -50,7 +97,6 @@ using (var scope = app.Services.CreateScope())
     db.Database.Migrate();
     db.SeedData();
 }
-
 
 app.UseSwagger();
 app.UseSwaggerUI(c =>
@@ -60,5 +106,9 @@ app.UseSwaggerUI(c =>
 });
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 app.Run();
